488dc0df74dda293a5d066fc29c84a3f57fdffeb
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-04-21 18:12:28 +0300

    init

---
+++ geometry.js
@@ -0,0 +1,76 @@
+let states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "name": "Н401",
+        "visible": false,
+        "coordinates": [[
+            [4,90 - 37], 
+            [4,1],
+            [58,1],
+            [58,90 - 38],
+            [51,90 - 38],
+            [51,90 - 43],
+            [10,90 - 43],
+            [10,90 - 37],
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "name": "Н402",
+        "visible": false,
+        "coordinates": [[
+            [3, 89],
+            [3, 72],
+            [15, 72],
+            [15,89],
+        ]]
+    }
+},{
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "name": "Н402",
+        "visible": false,
+        "coordinates": [[
+            [15, 89],
+            [15, 72],
+            [29.5, 72],
+            [29.5,89],
+        ]]
+    }
+},{
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "name": "Туалет",
+        "visible": false,
+        "coordinates": [[
+            [106, 89],
+            [106, 71],
+            [134, 71],
+            [134,89],
+        ]]
+    }
+},{
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "name": "Н405",
+        "visible": false,
+        "coordinates": [[
+            [106, 90 -28],
+            [106, 90 - 71],
+            [134, 90 - 71],
+            [134,90 -28],
+        ]]
+    }
+}
+];
---
+++ map.js
@@ -0,0 +1,106 @@
+var h = 400;
+var w = 400;
+var ibounds = [[0,0], [90,90]];
+
+let activeRoom;
+ 
+var map = L.map('map', {
+  zoomSnap: 0.001,
+  minZoom: -1.5,
+  tileSize: 512,
+  ratate: true,
+  crs: L.CRS.Simple,
+  rotate: true,
+  minZoom: 1,
+  maxZoom: 2.5,
+  maxBounds: [
+    [490, 370],
+    [-400, -100]
+],
+});
+
+addSearch('<input id="find"><button onClick="findRoom()">Найти</button>', map);
+
+function changeActive(layer) {
+  if (activeRoom) {
+    console.log(activeRoom);
+    activeRoom.setStyle({fillOpacity: 0});
+  }
+  if (activeRoom != layer) {
+    activeRoom = layer;
+    layer.setStyle({fillOpacity: 0.4});
+    focutTo(layer);
+  }else {
+    activeRoom = null;
+  }
+}
+
+var w = 270;
+var h = 90;
+var ibounds = [[0,0], [h, w]];
+ 
+L.imageOverlay('map.svg', ibounds).addTo(map);
+map.fitBounds(ibounds);
+
+function findRoom () {
+  let inputText = document.querySelector("#find");
+  let room = inputText.value.toUpperCase();
+  inputText.value = '';
+  let currentRoom = findRoomInArr(room);
+  if (currentRoom != false) {
+    changeActive(currentRoom);
+  }
+}
+
+function focutTo (el) {
+  map.fitBounds(el.getBounds());
+}
+
+function findRoomInArr (name) {
+  result = false;
+  states.forEach((geometry) => {
+    console.log(geometry.geometry);
+    if (geometry.geometry.name.toUpperCase() == name) {
+      result = geometry.parent;
+    }
+  });
+  return result;
+}
+
+function bindingEvents (feature, layer) {
+  layer.on('click', (e) => {
+    changeActive(layer);
+  })
+}
+
+function bindingPopup (feature, layer) {
+  layer.bindPopup(feature.geometry.name);
+}
+
+function onEachFeature (feature, layer) {
+  feature.parent = layer;
+  bindingPopup(feature, layer);
+  bindingEvents(feature, layer);
+}
+
+let geo = L.geoJSON(states, {style: {fillOpacity: 0, opacity: 0}, onEachFeature: onEachFeature}).addTo(map);
+console.log(geo);
+// zoom the map to the polygon
+
+//Windows
+function addSearch (el, map) {
+var info = L.control();
+
+	info.onAdd = function (map) {
+		this._div = L.DomUtil.create('div', 'info');
+    this._div.className = 'find-room';
+		this.update();
+		return this._div;
+	};
+
+	info.update = function () {
+		this._div.innerHTML = el;
+	};
+
+	info.addTo(map);
+}
