9705fc6aa37fd061af7bd83800d2880f6077521c
ProstakKotak <fineladder@gmail.com>
2021-04-25 23:03:37 +0300

    Замена движка для рендера на PhantomJS

--- polymap_api/api/views.py
+++ polymap_api/api/views.py
@@ -1,30 +1,22 @@
 # Django
 from django.shortcuts import render, get_object_or_404, Http404, redirect
 from django.views.generic import View
-from time import sleep # this should go at the top of the file
+from time import sleep
 
-# Selenium
+# Для скрапера
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
+import os
 
 class Index(View):
     def get(self, request):
-        url = "https://rasp.dmami.ru/?201-322"
 
-        options = Options()
-        options.headless = True
-        options.add_argument("--window-size=1920,1080")
-
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-        driver.get(url)
-
-        sleep(5)
-        driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
-
-        aud = driver.find_element_by_class_name('schedule-auditory').text
+        # Драйвер для рендера JS на странице расписания
+        driver = webdriver.PhantomJS(
+            service_log_path=os.path.devnull,
+            executable_path='C:/users/finel/desktop/phantomjs.exe'
+        )                  # /home/std/.local/bin/phantomjs для СЕРВЕРА
+        driver.get('https://rasp.dmami.ru/?201-322')
 
         return render(request, 'index.html', context={
-            'driver': aud,
+            'driver': driver.find_element_by_class_name('schedule-auditory').text,
         })
